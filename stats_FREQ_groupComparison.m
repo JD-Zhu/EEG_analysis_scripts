cad8b27ad71903b4d9013765109b03119fc892a8
Judy D Zhu <di.zhu@sydney.edu.au>
2021-09-02 01:26:22 +1000

    Added individual channel analysis

--- stats_FREQ_groupComparison.m
+++ stats_FREQ_groupComparison.m
@@ -1,20 +1,29 @@
 % SPECIFY the folder for this statistical analysis
 stats_folder = 'Z:\Analysis\Judy\EpisodicMigraine\stats\12vs12\';
 
+
 % location of freq results for each group:
 migraineurs_folder = [stats_folder 'GA_migraineurs\'];
 controls_folder = [stats_folder 'GA_controls\'];
+
+% load data
+load([migraineurs_folder 'GA_avg.mat']);
+mig_avg = GA_freq;
+load([controls_folder 'GA_avg.mat']);
+ctrl_avg = GA_freq;
+
+load([migraineurs_folder 'GA_individuals.mat']);
+mig_indi = GA_freq_indi; 
+load([controls_folder 'GA_individuals.mat']);
+ctrl_indi = GA_freq_indi;
 
 
 %% plot overall power (mig vs ctrl)
 x_limits = [2 30];
 
 figure; hold on;
-
-load([migraineurs_folder 'GA_avg.mat']);
-plot(GA_freq.freq, mean(GA_freq.powspctrm));
-load([controls_folder 'GA_avg.mat']);
-plot(GA_freq.freq, mean(GA_freq.powspctrm));
+plot(mig_avg.freq, mean(mig_avg.powspctrm));
+plot(ctrl_avg.freq, mean(ctrl_avg.powspctrm));
 
 xlim(x_limits);
 xlabel('Frequency (Hz)');
@@ -28,25 +37,48 @@ export_fig(gcf, [stats_folder 'overall_power_mig-vs-ctrl.png']);
 %% run t-test (mig vs ctrl) on a particular freq range
 freq_range = [8:10];
 
-load([migraineurs_folder 'GA_individuals.mat']);
-mig = squeeze(mean(GA_freq_indi.powspctrm, 2)); % avg over all channels
-load([controls_folder 'GA_individuals.mat']);
-ctrl = squeeze(mean(GA_freq_indi.powspctrm, 2)); % avg over all channels
+mig = squeeze(mean(mig_indi.powspctrm, 2)); % avg over all channels
+ctrl = squeeze(mean(ctrl_indi.powspctrm, 2)); % avg over all channels
 
 a = mean(mig(:,freq_range), 2); % avg over the selected freq range
 b = mean(ctrl(:,freq_range), 2); % avg over the selected freq range
 
-% this function conducts a two-samples t-test
+% this function conducts a two-sample t-test
 [h,p,ci,stats] = ttest2(a, b, 'Vartype','unequal'); % or should it be equal variance? (p-values are similar)
-p
+stats.tstat  % t-value
+p            % p-value
+
+
+%% individual channel analysis: 12vs12 t-test at each freq for each channel
+
+N_chan = size(mig_indi.powspctrm, 2); % number of channels
+N_freq = size(mig_indi.powspctrm, 3); % number of freqs
+
+% initialise "chan x freq" matrix to store t-values
+t_values = zeros(N_chan, N_freq);
+
+for i = 1:N_chan % loop through each channel
+    for j = 1:N_freq % loop through each freq
+        a = mig_indi.powspctrm(:,i,j); % extract power for all migraineurs
+        b = ctrl_indi.powspctrm(:,i,j); % extract power for all controls
+        
+        [h,p,ci,stats] = ttest2(a, b, 'Vartype','unequal'); % or should it be equal variance?
+        t_values(i,j) = stats.tstat; % store the t-value into the "chan x freq" matrix
+    end
+end
+
+imagesc(t_values)
+colorbar
+ylabel('EEG channel');
+xlabel('Frequency (Hz)');
+title('t-values (migraineurs > controls)');
 
 
 %% plot the topography difference btwn two groups (like in Flavia's paper)
 % e.g. subtract the topo for alpha (migraineurs minus controls)
-load([migraineurs_folder 'GA_avg.mat']);
-mig_avg = GA_freq;
-load([controls_folder 'GA_avg.mat']);
-ctrl_avg = GA_freq;
+
+% NOTE: this plot is based on absolute power diff in the GA, 
+% whereas Flavia plotted the t-values
 
 % calculate the difference GA
 diff = mig_avg;
